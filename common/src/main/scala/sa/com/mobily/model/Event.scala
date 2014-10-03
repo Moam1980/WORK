758ba4cde45ea08eb5bfa3e9efc404e79e1d5637
Raul Saez <rsaez@stratio.com>
2014-10-03 16:40:07 +0200

    Define unified format for all the events

--- common/src/main/scala/sa/com/mobily/model/Event.scala
+++ common/src/main/scala/sa/com/mobily/model/Event.scala
@@ -6,53 +6,20 @@ package sa.com.mobily.model
 
 /**
   */
-case class CSEvent(begin_time: Long,
-                   ci: String,
-                   eci: String,
-                   end_time: Long,
-                   event_type: Short,
-                   imei: Long,
-                   imsi: Long,
-                   ixc: String,
-                   lac: String,
-                   mcc: String,
-                   mnc: String,
-                   msisdn: Long,
-                   rac: String,
-                   rat: Short,
-                   sac: String,
-                   tac: String) {
-
-}
-
-
-/**
-  */
-case class PSEvent(begin_time: Long,
-                   ci: String,
-                   eci: String,
-                   end_time: Long,
-                   event_type: Short,
-                   imei: Long,
-                   imsi: Long,
-                   ixc: String,
-                   lac: String,
-                   mcc: String,
-                   mnc: String,
-                   msisdn: Long,
-                   prot_category: Short,
-                   prot_type: Short,
-                   rac: String,
-                   rat: Short,
-                   sac: String,
-                   server_ip: String,
-                   server_port: Short,
-                   tac: String) {
-
-}
-
-
-object Event {
-
-
-}
+case class CsEvent(
+  beginTime: Long,
+  ci: String,
+  eci: String,
+  endTime: Long,
+  eventType: Short,
+  imei: Long,
+  imsi: Long,
+  ixc: String,
+  lac: String,
+  mcc: String,
+  mnc: String,
+  msisdn: Long,
+  rac: String,
+  rat: Short,
+  sac: String,
+  tac: String)
