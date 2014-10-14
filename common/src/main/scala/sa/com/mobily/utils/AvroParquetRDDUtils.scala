f6cb8d8aee5f99c1db1709cb0180bc9b76ea1b96
Raul Saez <rsaez@stratio.com>
2014-10-14 17:17:44 +0200

    Implement and run the transformation of events into the unified format #18
    We have developed an utility for write a RDD to Parquet. So we can test the utility that  we have  made we have created
    a Scala test.
    
    The test read from a XDR csv with near 15 million rows and use an Event Scheme for serialize an Avro file into Parquet.
    We have made writes in Parquet without Avro so we can compare sizes and performance.
    
    Test has been executed over a local filesystem and distributed HDFS too.
    
    Next steps should be discussed between team members.

---
+++ common/src/main/scala/sa/com/mobily/utils/AvroParquetRDDUtils.scala
@@ -0,0 +1,60 @@
+/*
+ * TODO: License goes here!
+ */
+
+package sa.com.mobily.utils
+
+import java.io.File
+
+import org.apache.avro.file.DataFileWriter
+import org.apache.avro.specific.{SpecificDatumWriter, SpecificRecord}
+import org.apache.hadoop.mapred.JobConf
+import org.apache.hadoop.mapreduce.Job
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.rdd.RDD
+import parquet.avro.{AvroParquetOutputFormat, AvroReadSupport, AvroWriteSupport}
+import parquet.hadoop.{ParquetInputFormat, ParquetOutputFormat}
+
+import scala.reflect.ClassTag
+
+/**
+ * Generic utility class for Avro Parquet
+ */
+object AvroParquetRDDUtils {
+
+
+  /**
+   * Read in a parquet file containing Avro data and return the result as an RDD.
+   *
+   * @param sc The SparkContext to use
+   * @param parquetFile The Parquet input file assumed to contain Avro objects
+   * @return An RDD that contains the data of the file
+   */
+  def readParquetRDD[T <% SpecificRecord](sc: SparkContext, parquetFile: String)(implicit tag: ClassTag[T]): RDD[T] = {
+    val jobConf = new JobConf(sc.hadoopConfiguration)
+    ParquetInputFormat.setReadSupportClass(jobConf, classOf[AvroReadSupport[T]])
+    sc.newAPIHadoopFile(
+      parquetFile,
+      classOf[ParquetInputFormat[T]],
+      classOf[Void],
+      tag.runtimeClass.asInstanceOf[Class[T]],
+      jobConf)
+      .map(_._2.asInstanceOf[T])
+  }
+
+  def writeParquetRDD[T <% SpecificRecord](sc: SparkContext, rdd: RDD[(Void, T)], schema: org.apache.avro.Schema, parquetDir: String)(implicit tag: ClassTag[T]) = {
+    val jobConf= new Job(sc.hadoopConfiguration)
+    // Configure the ParquetOutputFormat to use Avro as the serialization format
+    ParquetOutputFormat.setWriteSupportClass(jobConf, classOf[AvroWriteSupport])
+    // You need to pass the schema to AvroParquet when you are writing objects but not when you
+    // are reading them. The schema is saved in Parquet file for future readers to use.
+    AvroParquetOutputFormat.setSchema(jobConf, schema)
+    // Save the RDD to a Parquet file in our temporary output directory
+
+    rdd.saveAsNewAPIHadoopFile(parquetDir, classOf[Void], tag.runtimeClass.asInstanceOf[Class[T]],
+      classOf[ParquetOutputFormat[T]], jobConf.getConfiguration)
+
+  }
+
+}
---
+++ common/src/test/scala/sa/com/mobily/utils/AvroParquetRDDUtils$Test.scala
@@ -0,0 +1,61 @@
+package sa.com.mobily.utils
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkConf, SparkContext}
+import org.scalatest.{BeforeAndAfterEach, FunSuite}
+import sa.com.mobily.event.{CsEvent, Event}
+
+/**
+ * Created by rsaez on 13/10/14.
+ */
+class AvroParquetRDDUtils$Test extends FunSuite with BeforeAndAfterEach {
+
+  val csvFile: String = "file:///home/rsaez/tmp/kk.csv"
+
+  @transient var sqc: SQLContext = _
+
+  override def beforeEach() {
+    println("creating spark context")
+    val conf =
+      new SparkConf(false)
+        //.setMaster("spark://172.19.0.215:7077")
+        .setMaster("local[8]")
+        .setAppName("test")
+    sqc = new SQLContext(new SparkContext(conf))
+
+  }
+
+  private def resetSparkContext(): Unit = {
+
+    if (sqc != null) {
+      println("stopping Spark Context")
+      sqc.sparkContext.stop()
+    }
+    sqc = null
+
+  }
+
+  override def afterEach() {
+    resetSparkContext()
+  }
+
+  test("Read from CSV and Write to Parquet") {
+
+    val map: RDD[CsEvent] = sqc.sparkContext.textFile(csvFile).map(line => {
+      Event.fromCsv.fromFields(line.split(","))
+    })
+
+
+    //sqc.applySchema(sqc.createSchemaRDD(map), StructType(Event.getFields())).saveAsParquetFile("/tmp/test/file" + System.currentTimeMillis())
+
+    sqc.createSchemaRDD(map).saveAsParquetFile("file:///tmp/parquet_only" + System.currentTimeMillis())
+    //sqc.createSchemaRDD(map).saveAsParquetFile("hdfs://10.200.0.49:8020/user/rsaez/parquet_only" + System.currentTimeMillis())
+    println("####### JOB 1 FINISHED")
+
+    //    AvroParquetRDDUtils.writeParquetRDD(sqc.sparkContext, map.map[(Void, CsEvent)](event => (null, event)), Event.SCHEMA$, "hdfs://10.200.0.49:8020/user/rsaez/parquet" + System.currentTimeMillis())
+    //    println("####### JOB 2 FINISHED")
+
+  }
+
+}
