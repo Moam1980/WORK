4398cf4c334c379c6af7b033758ed614c02b92fa
jlopezmalla <jlopezm@stratio.com>
2014-11-24 11:37:47 +0100

    Flickering detector for Patterns AXA

--- common/src/main/scala/sa/com/mobily/cell/Flickering.scala
+++ common/src/main/scala/sa/com/mobily/cell/Flickering.scala
@@ -4,14 +4,33 @@
 
 package sa.com.mobily.cell
 
+import annotation.tailrec
+
 object Flickering {
 
-  def detect(byUserSortedCells: Seq[(Long, (Int, Int))], timeElapsed: Long): Seq[Set[(Int, Int)]] = {
-    byUserSortedCells.sliding(3).filter({
-      case Seq(a, b, c) => (a._2 == c._2 && b._2 != a._2 && (c._1 - a._1) <= timeElapsed)
-      case _ => false
-    }).map({
-      case Seq(a, b, c) => Set(a._2, b._2)
-    }).toSeq.distinct
+  def detect(byUserSortedCells: Seq[(Long, (Int, Int))], timeWindow: Long): Set[Set[(Int, Int)]] = {
+    
+    @tailrec
+    def detect(
+        byUserSortedTimeCells: Seq[(Long, (Int, Int))],
+        timeElapsed: Long,
+        result: Set[Set[(Int, Int)]] = Set()): Set[Set[(Int, Int)]] = {
+      if (byUserSortedTimeCells == Nil) result
+      else {
+        val cellAnalysis = byUserSortedTimeCells.head._2
+        val cellsToAnalysis = byUserSortedTimeCells.tail
+        val timeMax = byUserSortedTimeCells.head._1 + timeElapsed
+        val timeCellsWithFlickering = cellsToAnalysis.take(cellsToAnalysis.lastIndexWhere(timeCell =>
+          timeCell._1 <= timeMax && timeCell._2 == cellAnalysis))
+        if (timeCellsWithFlickering.isEmpty) detect(byUserSortedTimeCells.tail, timeElapsed, result)
+        else {
+          val cellsWithFlickering = timeCellsWithFlickering.map(timeCell => timeCell._2).filter(cell =>
+            cell != cellAnalysis).map(cell => Set(cellAnalysis, cell))
+          detect(byUserSortedTimeCells.tail, timeElapsed, result ++ cellsWithFlickering)
+        }
+      }
+    }
+
+    detect(byUserSortedCells, timeWindow)
   }
 }
--- common/src/test/scala/sa/com/mobily/cell/flickering/FlickeringTest.scala
+++ common/src/test/scala/sa/com/mobily/cell/flickering/FlickeringTest.scala
@@ -10,25 +10,68 @@ import sa.com.mobily.cell.Flickering
 
 class FlickeringTest extends FlatSpec with ShouldMatchers {
 
-  trait WithFlickeringCells {
+  trait WithCells {
 
-    val cell1 = (1L, (1, 1))
-    val cell2 = (2L, (2, 2))
-    val cell3 = (3L, (1, 1))
-    val cell4 = (4L, (2, 2))
-    val cell5 = (5L, (1, 3))
-    val cell6 = (7L, (2, 2))
-    val cell7 = (7L, (1, 3))
-    val cell8 = (8L, (1, 1))
-
-    val cells = List(cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8)
-    val cellsSet1 = Set(cell1._2, cell2._2)
-    val cellsSet2 = Set(cell5._2, cell6._2)
+    val cell1 = (1, 1)
+    val cell2 = (2, 2)
+    val cell3 = (1, 3)
+    val cell4 = (2, 3)
+    val cell5 = (3, 3)
+    val cell6 = (3, 4)
   }
 
-  "Flickering" should "detect flickering" in new WithFlickeringCells {
-    val flick = Flickering.detect(cells, 2)
-    flick.length should be (2)
-    flick should be (Seq(cellsSet1, cellsSet2))
+  trait WithFlickeringCells extends WithCells {
+
+    val timeCell1 = (1L, cell1)
+    val timeCell2 = (2L, cell2)
+    val timeCell3 = (3L, cell1)
+    val timeCell4 = (4L, cell2)
+    val timeCell5 = (5L, cell3)
+    val timeCell6 = (7L, cell2)
+    val timeCell7 = (7L, cell3)
+    val timeCell8 = (8L, cell1)
+
+    val timeCells = List(timeCell1, timeCell2, timeCell3, timeCell4, timeCell5, timeCell6, timeCell7, timeCell8)
+    val flickeringCells = Set(Set(cell1, cell2), Set(cell2, cell3), Set(cell1,cell3))
+  }
+
+  trait WithOneRepeatedCell extends WithCells {
+
+    val timeCell1 = (1L, cell1)
+    val timeCell2 = (2L, cell1)
+    val timeCell3 = (3L, cell1)
+    val timeCell4 = (4L, cell1)
+    val timeCell5 = (5L, cell1)
+    val timeCell6 = (6L, cell2)
+
+    val timeCells = List(timeCell1, timeCell2, timeCell3, timeCell4, timeCell5, timeCell6)
+  }
+
+  trait WithNonFlickeringCells extends WithCells {
+
+    val timeCell1 = (1L, cell1)
+    val timeCell2 = (2L, cell2)
+    val timeCell3 = (3L, cell3)
+    val timeCell4 = (4L, cell4)
+    val timeCell5 = (5L, cell5)
+    val timeCell6 = (6L, cell6)
+
+    val timeCells = List(timeCell1, timeCell2, timeCell3, timeCell4, timeCell5, timeCell6)
+  }
+
+  "FlickeringDetector" should "detect flickering" in new WithFlickeringCells {
+    val flickering = Flickering.detect(timeCells, 5)
+    flickering.size should be (3)
+    flickering should be (flickeringCells)
+  }
+
+  it should "not detect flickering in range with one cell" in new WithOneRepeatedCell {
+    val flickering = Flickering.detect(timeCells, 5)
+    flickering.size should be (0)
+  }
+
+  it should "not detect flickering in range with non flickering cells" in new WithNonFlickeringCells  {
+    val flickering = Flickering.detect(timeCells, 5)
+    flickering.size should be (0)
   }
 }
