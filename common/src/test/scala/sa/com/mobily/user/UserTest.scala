c30d410f07d3be47918ad7ca8f3adac17aff3dd5
Alberto Rodriguez <albertostratio@stratio.com>
2014-12-11 18:36:18 +0100

    Users comparison logic implemented

--- common/src/test/scala/sa/com/mobily/user/UserTest.scala
+++ common/src/test/scala/sa/com/mobily/user/UserTest.scala
@@ -16,8 +16,17 @@ class UserTest extends FlatSpec with ShouldMatchers {
       msisdn = 560917079L)
 
     val userFields = Array[String]("866173010386736", "420034122616618", "560917079", "420", "03")
-
     val header = Array[String]("imei", "imsi", "msisdn", "mcc", "mnc")
+  }
+
+  trait WithEqualityUsers {
+
+    val firstUserImei = User("imei", "firstImsi", 1)
+    val secondUserImei = firstUserImei.copy(imsi = "secondImsi", msisdn = 2)
+    val firstUserImsi = User("firstImei", "imsi", 1)
+    val secondUserImsi = firstUserImsi.copy(imei = "secondImei", msisdn = 2)
+    val firstUserMsisdn = User("firstImei", "firstImsi", 1)
+    val secondUserMsisdn = firstUserMsisdn.copy(imei = "secondImei", imsi = "secondImsi")
   }
 
   "User" should "identify MCC from IMSI" in new WithUser {
@@ -33,10 +42,38 @@ class UserTest extends FlatSpec with ShouldMatchers {
   }
 
   it should "generate fields" in new WithUser {
-    User.fields(user) should be (userFields)
+    user.fields should be (userFields)
   }
 
   it should "generate header" in new WithUser {
     User.header should be (header)
+  }
+
+  it should "return the same number of fields for header and fields method" in new WithUser {
+    User.header.length == user.fields.length should be(true)
+  }
+
+  it should "throw an Exception when it does not receive neither msisdn nor imsi nor imei" in {
+    an [Exception] should be thrownBy User("", "", 0)
+  }
+
+  it should "compare to true two users with the same imei" in new WithEqualityUsers {
+    firstUserImei == secondUserImei should be(true)
+    firstUserImei.equals(secondUserImei) should be(true)
+  }
+
+  it should "compare to true two users with the same imsi" in new WithEqualityUsers {
+    firstUserImsi == secondUserImsi should be(true)
+    firstUserImsi.equals(secondUserImsi) should be(true)
+  }
+
+  it should "compare to true two users with the same msisdn" in new WithEqualityUsers {
+    firstUserMsisdn == secondUserMsisdn should be(true)
+    firstUserMsisdn.equals(secondUserMsisdn) should be(true)
+  }
+
+  it should "compare to false two users with different fields" in new WithEqualityUsers {
+    firstUserImei == secondUserImsi should be(false)
+    firstUserImei.equals(secondUserImsi) should be(false)
   }
 }
