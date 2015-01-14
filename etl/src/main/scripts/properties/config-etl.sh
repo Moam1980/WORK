ae6edca8a5e124e0006af14098fb794853c7d971
vmrh <vm.rustarazo@gmail.com>
2015-01-14 14:32:43 +0300

    Correct directories for CS and roaming

--- etl/src/main/scripts/properties/config-etl.sh
+++ etl/src/main/scripts/properties/config-etl.sh
@@ -25,7 +25,7 @@ DB_PASSWORD="isop"
 # Configuration for hadoop file size output
 HADOOP_OUTPUT_FILE_PATH=${OUTPUT_FILE_PATH}/hadoop
 HADOOP_PATH=hdfs://10.64.247.224/user/tdatuser/
-HADOOP_FILES_SIZE_FILE=~/Mobily-Data/cs_probes/hadoop_files_summary.csv
+HADOOP_FILES_SIZE_FILE=/data/landing/cs-probes/hadoop_files_summary.csv
 
 # CS hadoop configuration
 HADOOP_CS_PROBES_OUTPUT_FILE_PATH=${HADOOP_OUTPUT_FILE_PATH}/cs-probes
@@ -34,7 +34,7 @@ HADOOP_CS_PROBES_OUTPUT_FILE_PATH=${HADOOP_OUTPUT_FILE_PATH}/cs-probes
 CS_USER="edm"
 CS_SERVER="10.64.9.35"
 CS_LANDING_AREA="/data/landing/cs-probes"
-CS_DIRECTORIES=("A-Interface" "IuCS-Interface")
+CS_DIRECTORIES=("A-Interface" "IUCS-Interface")
 CS_DATA_SEPARATOR=("" ".")
 CS_FILE_PREFIXES=("ADR_AINT_6-" "TDR-IU-7-")
 CS_FILE_EXTENSIONS=(".gz" ".gz")
