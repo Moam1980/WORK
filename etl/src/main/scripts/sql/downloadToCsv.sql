ecf4ce9a0927910ed3a266154f09ebefc4d9bca1
vmrh <vm.rustarazo@gmail.com>
2015-01-20 11:52:29 +0300

    Solved problem writting in the console

--- etl/src/main/scripts/sql/downloadToCsv.sql
+++ etl/src/main/scripts/sql/downloadToCsv.sql
@@ -1,15 +1,17 @@
-set echo off
-set feedback off
-set linesize %%LINE_SIZE%%
-set pagesize 0
-set sqlprompt ''
-set trimspool on
+set echo off;
+set feedback off;
+set linesize %%LINE_SIZE%%;
+set pagesize 0;
+set sqlprompt '';
+set trimspool on;
+set headsep off;
+set termout off;
 
-spool %%OUTPUT_FILE%%
+spool %%OUTPUT_FILE%%;
 
 %%SQL_QUERY%%
 %%SQL_CONDITION%%;
 
-spool off
+spool off;
 
-exit
+exit;
