1df806e1f5645120ef21164c99b42b1874f8d54f
Sergio Gómez <sgomez@stratio.com>
2015-02-27 11:57:50 +0100

    Row functions for read a period from parquet

---
+++ common/src/main/scala/sa/com/mobily/utils/spark/rddRowDsl.scala
@@ -0,0 +1,43 @@
+/*
+ * TODO: License goes here!
+ */
+
+package sa.com.mobily.utils.spark
+
+import scala.annotation.tailrec
+import scala.language.implicitConversions
+
+import com.github.nscala_time.time.Imports._
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{Row, SQLContext}
+
+import sa.com.mobily.utils.EdmCoreUtils
+
+class RddRowFunctions(self: SQLContext) {
+
+  @tailrec
+  final def readFromPeriod(path: String, start: DateTime, end: DateTime,
+      result: RDD[Row] = self.sparkContext.emptyRDD): RDD[Row] = {
+    if (start == end) result.union(self.parquetFile(RddRowDsl.replaceDate(path, start)))
+    else
+      readFromPeriod(path, start.plusDays(1), end, result.union(self.parquetFile(RddRowDsl.replaceDate(path, start))))
+  }
+
+  def readFromPeriod(path: String, start: String, end: String): RDD[Row] =
+    readFromPeriod(path, RddRowDsl.getDateFromString(start), RddRowDsl.getDateFromString(end))
+}
+
+trait RddRowDsl {
+
+  implicit def rddRowFunctions(self: SQLContext): RddRowFunctions = new RddRowFunctions(self)
+}
+
+object RddRowDsl extends RddRowDsl {
+
+  val DateRegex = "\\d{4}\\/\\d{2}\\/\\d{2}".r
+
+  def replaceDate(path: String, dateTime: DateTime): String =
+    DateRegex.replaceFirstIn(path, EdmCoreUtils.dateAsString(dateTime.getMillis))
+
+  def getDateFromString(dateAsString: String): DateTime = EdmCoreUtils.FmtDate.parseDateTime(dateAsString)
+}
